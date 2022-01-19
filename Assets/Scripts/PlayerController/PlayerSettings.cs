d2aaadf4248003394fd3763e97547a7651ec2fe0
Snoweuph <domsaeume@gmail.com>
2022-01-19 01:16:10 +0100

    Initiate Project

---
+++ Assets/Scripts/PlayerController/PlayerSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerSettings", menuName = "Player/PlayerSettings", order = 1)]
+public class PlayerSettings : ScriptableObject
+{
+    [Header("Walking and Running")]
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
+}
