033ef6077d0fcc217e502042d9cd2c04bdd58ddb
Snoweuph <domsaeume@gmail.com>
2022-01-19 20:55:32 +0100

    started  implementing task system (ScriptableObj)

---
+++ Assets/Scripts/NPCMoodSystem/NPC.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NPC", menuName = "NPC/NPC", order = 1)]
+public class NPC : ScriptableObject
+{
+    public string _name;
+    [TextArea]
+    public string description;
+    public List<NPCTaskBlueprint> tasks = new List<NPCTaskBlueprint>();
+
+    //Function to Recive the Callback that a Task has been Completed
+    public void CompleteTask(NPCTask task, bool positiveSucess)
+    {
+        Debug.Log("The Task: " + task._name + " has been Completed for the NPC: " + _name);
+    }
+}
---
+++ Assets/Scripts/NPCMoodSystem/NPCTaskBlueprint.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class NPCTaskBlueprint
+{
+    public NPCTask task;
+    public float positiveReward;
+    public float negativeReward;
+
+}
