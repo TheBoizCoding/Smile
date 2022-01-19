54fc4944d817237f0b24cc0267de92f92593b4e1
Snoweuph <domsaeume@gmail.com>
2022-01-19 09:15:45 +0100

    Working On PlayerController

--- Assets/Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -1,10 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    //Manual Refrences
     [SerializeField] private PlayerSettings playerSettings;
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private Transform groundCheckPosition;
+    //Automatic Refrences
+    private Rigidbody rb;
+    //Awake
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            Debug.LogError("PlayerController: Rigidbody not found!");
+        }
+    }
+
 
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/PlayerController/PlayerSettings.cs
+++ Assets/Scripts/PlayerController/PlayerSettings.cs
@@ -5,7 +5,22 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerSettings", menuName = "Player/PlayerSettings", order = 1)]
 public class PlayerSettings : ScriptableObject
 {
-    [Header("Walking and Running")]
+    [Header("Layers")]
+    public LayerMask groundLayer;
+    [Header("Ground Detection")]
+    public float groundCheckRadius = 0.25f;
+
+    [Header("Walking, Running, Sprinting")]
     public float walkSpeed = 5f;
     public float runSpeed = 10f;
+    public float sprintSpeed = 15f;
+    public float diferenceStraveSpeed = 0.5f;
+    public float acceleration = 0.5f;
+    public float deceleration = 0.5f;
+
+    [Header("Crouching")]
+    public float someSetting = 2f;
+
+    [Header("Jumping")]
+    public float jumpForce = 10f;
 }
